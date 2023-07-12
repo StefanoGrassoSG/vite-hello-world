aaf8479beed85eb869ada5fe2c9f45a12129d64a
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-07-12 17:37:08 +0200

    ADD mainheader component

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import MainHeader from './components/MainHeader.vue'
+
+export default {
+  data() {
+    return { 
+
+     }
+  },
+  components: {
+    MainHeader
+  },
+}
+</script>
+
+<template>
+  <MainHeader/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return { 
+
+     }
+  },
+}
+</script>
+
+<template>
+    <header>
+        La mia prima app con Vite!
+    </header>
+</template>
+
+<style scoped>
+header {
+    color: green;
+    font-size: 50px;
+    font-family: 'Courier New', Courier, monospace;
+    font-weight: bold;
+}
+</style>
